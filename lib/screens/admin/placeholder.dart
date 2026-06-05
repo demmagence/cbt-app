963eb3408ac8ca602df5aa087fa7b9bb033ec8ed
wibisanabama <wibisanabama@gmail.com>
2026-06-06 06:35:35 +0700

    feat(admin): implement Create User form for Guru and Siswa (#14)

--- lib/screens/admin/placeholder.dart
+++ lib/screens/admin/placeholder.dart
@@ -1,16 +1,5 @@
 import 'package:flutter/material.dart';
 
-class CreateUserScreen extends StatelessWidget {
-  const CreateUserScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Create User Screen')),
-    );
-  }
-}
-
 class StatisticsScreen extends StatelessWidget {
   const StatisticsScreen({super.key});
 
