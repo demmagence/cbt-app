8eceaa6df2a48f09e7d5e7140c01a94108207d62
Derylfabiensyah <derylfabien09@gmail.com>
2026-06-05 18:31:17 +0700

    feat: implement Siswa Exam History screen and state management

--- lib/screens/siswa/placeholder.dart
+++ lib/screens/siswa/placeholder.dart
@@ -3,17 +3,6 @@ import 'package:flutter/material.dart';
 
 
 
-class ExamHistoryScreen extends StatelessWidget {
-  const ExamHistoryScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Exam History Screen')),
-    );
-  }
-}
-
 class SiswaProfileScreen extends StatelessWidget {
   const SiswaProfileScreen({super.key});
 
