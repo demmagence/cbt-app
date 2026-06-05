01357118b5cf41a8273c367f8e017cc7ddaa174e
wibisanabama <wibisanabama@gmail.com>
2026-06-05 14:06:09 +0700

    feat(firebase): Setup Firebase project, auth, and firestore (#1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
@@ -12,7 +18,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('CBT App - Firebase Initialized!'),
         ),
       ),
     );
